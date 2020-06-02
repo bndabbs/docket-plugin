9b2f88749eb18bba1de6dddf677a89a9ca241862
Bradford Dabbs <bradford.dabbs@elastic.co>
2020-06-02 13:14:16 -0500

    Update package dependencies and scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   root: true,	
   extends: [
     '@elastic/eslint-config-kibana',
-    'plugin:@elastic/eui/recommended',
-    'plugin:react-hooks/recommended'
+    'plugin:@elastic/eui/recommended'
   ],
 };
